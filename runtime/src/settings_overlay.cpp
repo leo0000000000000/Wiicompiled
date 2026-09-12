e34cece38146dd3dc86190c0728133567000cba1
leo0000000000000 <leonardomugnaini03@gmail.com>
2026-09-12 18:49:52 +0200

    Fix major Rebinding bug

--- runtime/src/settings_overlay.cpp
+++ runtime/src/settings_overlay.cpp
@@ -1504,9 +1504,7 @@ void DrawTopBar() {
 
     if (ImGui::BeginMenu("Controller settings")) {
         DrawControllerSettings();
-        // Nest capture under this menu so opening/closing the modal preserves
-        // the settings popup and its current port and scroll position.
-        DrawRebindPrompt();
+
         ImGui::EndMenu();
     }
     // Top bar menu separator
